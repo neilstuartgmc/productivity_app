ef287baa2b3b51b8d4e31687ec8ace293d64aaaa
neilstuartgmc <139452824+neilstuartgmc@users.noreply.github.com>
2023-07-17 15:08:33 +0100

    Update PE_Productivity.py

--- PE_Productivity.py
+++ PE_Productivity.py
@@ -22,7 +22,7 @@ df["Weekending1"] = df["Weekending"].dt.date
 # ---- SIDEBAR ----
 
 # Sidebar Logo
-image = Image.open("Image/GMClogo.png")
+image = Image.open("images/GMClogo.png")
 st.sidebar.image(image, use_column_width=True)
 
 
