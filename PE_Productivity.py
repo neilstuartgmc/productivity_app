fd235802ff0fd61430e4ee5d61410ae32100e7f0
neilstuartgmc <139452824+neilstuartgmc@users.noreply.github.com>
2023-07-18 08:45:31 +0100

    Update PE_Productivity.py

--- PE_Productivity.py
+++ PE_Productivity.py
@@ -143,9 +143,7 @@ fig_product_sales.update_layout(
     xaxis=(dict(showgrid=False))
 )
 
-sales_value_by_cell = (
-    df_filtered_sidebar.groupby(by=["Resource"]).sum()[["PR Total Cost"]]
-)
+sales_value_by_cell = df_filtered_sidebar.groupby(by=["Resource"]).sum()[["PR Total Cost"]]
 fig_sales_value_by_cell = px.bar(
     sales_value_by_cell,
     x="PR Total Cost",
